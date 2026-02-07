845640e93a271a6eaed002ba3e176749b07452a8
Jakub Trzebiatowski <jakub.trzebiatowski+git@proton.me>
2026-02-07 12:45:11 +0100

    Add support for specifying a base branch in `git_add_missing_signatures.py`

--- tiny_git_cli_tools/git_add_missing_signatures.py
+++ tiny_git_cli_tools/git_add_missing_signatures.py
@@ -16,6 +16,11 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument('--repo-path', default='.', help='Path to the root of the Git repository')
     parser.add_argument('--remote-name', default='origin', help='Name of the remote to push changes to')
     parser.add_argument('--push-changes', action='store_true', help='Push rewritten branch to remote')
+    parser.add_argument(
+        '--base-branch',
+        default=None,
+        help='Base branch to rewrite until (e.g., origin/main). If provided, rewrites all commits until the merge base instead of stopping at the first unsigned commit.'
+    )
 
     return parser.parse_args()
 
@@ -39,13 +44,40 @@ def main() -> None:
         print('Error: Detached HEAD is not supported.', file=sys.stderr)
         sys.exit(1)
 
-    rewrite_branch(
-        repo=repo,
-        branch=branch,
-        new_author=None,
-        should_sign=True,
-        should_rewrite=lambda commit: not commit.gpgsig,
-    )
+    if args.base_branch:
+        try:
+            base_commit = repo.commit(args.base_branch)
+        except ValueError:
+            print(f'Error: Could not find base branch: {args.base_branch}', file=sys.stderr)
+            sys.exit(1)
+
+        merge_bases = repo.merge_base(branch.commit, base_commit)
+
+        if not merge_bases:
+            print(f'Error: No common ancestor found between {branch.name} and {args.base_branch}', file=sys.stderr)
+            sys.exit(1)
+
+        merge_base_sha = merge_bases[0].hexsha
+
+        print(f'Will rewrite commits until merge base: {merge_base_sha[:8]}')
+
+        rewrite_branch(
+            repo=repo,
+            branch=branch,
+            new_author=None,
+            should_sign=True,
+            should_rewrite=lambda commit: commit.hexsha != merge_base_sha
+        )
+    else:
+        print('Will rewrite all unsigned commits until the first signed commit.')
+
+        rewrite_branch(
+            repo=repo,
+            branch=branch,
+            new_author=None,
+            should_sign=True,
+            should_rewrite=lambda commit: not commit.gpgsig,
+        )
 
     print('Successfully signed all unsigned commits.')
 
